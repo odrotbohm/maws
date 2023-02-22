560ec717607d3df3f443205b3f7931f3ba691550
Oliver Drotbohm <info@odrotbohm.de>
2023-02-22 16:15:26 +0100

    Rewrite EventPublicationRegistryIntegrationTests to use Scenario API.

--- acme-commerce/src/test/java/com/acme/commerce/inventory/EventPublicationRegistryIntegrationTests.java
+++ acme-commerce/src/test/java/com/acme/commerce/inventory/EventPublicationRegistryIntegrationTests.java
@@ -17,82 +17,68 @@ package com.acme.commerce.inventory;
 
 import static org.assertj.core.api.Assertions.*;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.context.logging.LoggingApplicationListener;
-import org.springframework.boot.env.EnvironmentPostProcessorApplicationListener;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.boot.test.context.TestComponent;
-import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.modulith.ApplicationModuleListener;
 import org.springframework.modulith.events.EventPublicationRegistry;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.event.TransactionalEventListener;
+import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.modulith.test.Scenario;
 
-import com.acme.commerce.AcmeCommerce;
 import com.acme.commerce.catalog.Product.ProductIdentifier;
 import com.acme.commerce.core.Quantity;
+import com.acme.commerce.inventory.EventPublicationRegistryIntegrationTests.SomeTestEventListener;
 import com.acme.commerce.inventory.InventoryEvents.StockShort;
+import com.acme.commerce.order.OrderManagement;
 
 /**
  * @author Oliver Drotbohm
  */
+@ApplicationModuleTest
+@Import(SomeTestEventListener.class)
 @RequiredArgsConstructor
 class EventPublicationRegistryIntegrationTests {
 
+	private final SomeTestEventListener listener;
+	private final EventPublicationRegistry registry;
+
+	@MockBean OrderManagement orders;
+
 	@Test
-	void logsFailedEventPublicationsOnClose() {
+	void logsFailedEventPublicationsOnClose(Scenario scenario) {
 
-		new ApplicationContextRunner()
-				.withInitializer(new ConfigDataApplicationContextInitializer())
-				.withInitializer(context -> {
-					context.addApplicationListener(new EnvironmentPostProcessorApplicationListener());
-					context.addApplicationListener(new LoggingApplicationListener());
-				})
-				.withUserConfiguration(AcmeCommerce.class)
-				.withBean(SomeTestEventListener.class)
-				.withBean(SomeComponent.class)
-				.withPropertyValues("logging.level=WARN")
-				.run(context -> {
+		var productIdentifier = new ProductIdentifier(UUID.randomUUID());
+		var item = new InventoryItem(productIdentifier, Quantity.of(10));
 
-					context.getBean(SomeComponent.class).someMethod();
-
-					Thread.sleep(40);
-
-					var registry = context.getBean(EventPublicationRegistry.class);
-					var publications = registry.findIncompletePublications();
-
-					assertThat(publications).hasSize(1);
+		scenario.publish(StockShort.of(item, Quantity.of(15)))
+				.andWaitForStateChange(() -> listener.getEx())
+				.andVerify(__ -> {
+					assertThat(registry.findIncompletePublications()).hasSize(1);
 				});
+
 	}
 
-	@TestComponent
-	@RequiredArgsConstructor
-	static class SomeComponent {
-
-		private final ApplicationEventPublisher publisher;
-
-		@Transactional
-		public void someMethod() {
-
-			var productIdentifier = new ProductIdentifier(UUID.randomUUID());
-			var item = new InventoryItem(productIdentifier, Quantity.of(10));
-
-			publisher.publishEvent(StockShort.of(item, Quantity.of(15)));
-		}
-	}
-
-	@TestComponent
 	static class SomeTestEventListener {
 
-		@Async
-		@TransactionalEventListener
-		void on(StockShort event) {
-			throw new IllegalStateException("¯\\_(ツ)_/¯");
+		private @Getter RuntimeException ex;
+
+		@ApplicationModuleListener
+		void on(StockShort event) throws Exception {
+
+			Thread.sleep(1000);
+
+			var ex = new IllegalStateException("¯\\_(ツ)_/¯");
+
+			try {
+				throw ex;
+			} finally {
+				this.ex = ex;
+			}
 		}
 	}
 }
